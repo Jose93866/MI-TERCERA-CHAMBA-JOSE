cb284d27fd038e85db5cd1bc302593da8a98bf6a
Jose93866 <veronicatobonsena@gmail.com>
2024-06-20 14:27:23 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,115 @@
+
+
+
+function setup() {
+  createCanvas(1000, 400);
+  //Crea los sprites necesarios para formar tu nombre
+  //LETRA J
+  squareLeft1 = createSprite(90,250,110,20);
+  squareLeft1.shapeColor="#865797";
+  squareRight1 = createSprite(155,170,20,180);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(155,50,25,25);
+  squareRight1.shapeColor="#865797";
+  //|LETRA O
+  squareRight1 = createSprite(350,50,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(375,50,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(405,80,30,30); 
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(320,80,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(290,110,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(435,110,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(290,140,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(435,140,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(290,170,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(435,170,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(405,200,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(320,200,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(355,230,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(370,230,30,30);
+  squareRight1.shapeColor="#865797";
+  //LETRA S
+  squareRight1 = createSprite(570,50,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(585,50,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(540,80,30,30); 
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(510,110,30,30); 
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(510,140,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(510,170,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(540,200,30,30); 
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(550,200,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(570,200,30,30); 
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(580,200,30,30);
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(600,230,30,30); 
+  squareRight1.shapeColor="#865797";
+  squareRight1 = createSprite(630,260,30,30);
+  squareRight1.shapeColor="#865797";
+  squareLeft1 = createSprite(600,290,30,30); 
+  squareLeft1.shapeColor="#865797";
+  squareLeft1 = createSprite(580,310,30,30);
+  squareLeft1.shapeColor="#865797";
+  //LETRA E
+  squareLeft1 = createSprite(750,250,110,20);
+  squareLeft1.shapeColor="#865797";
+  squareRight1 = createSprite(700,170,20,180);
+  squareRight1.shapeColor="#865797";
+  squareLeft1 = createSprite(750,170,110,20);
+  squareLeft1.shapeColor="#865797";
+  squareLeft1 = createSprite(750,90,100,20);
+  squareLeft1.shapeColor="#865797";
+  
+  
+  
+
+
+
+  
+  
+  
+
+
+
+  
+  
+  
+  
+  
+
+  
+
+
+
+}
+
+
+function draw() {
+  background("black");
+
+  drawSprites();
+  
+}
+
+function preload(){
+
+}
